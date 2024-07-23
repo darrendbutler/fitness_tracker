dc2c0a6271d0df8c7ab8a442eb47e9af8ecb3701
Darren Butler <42709036+darrendbutler@users.noreply.github.com>
2024-07-23 11:25:17 -0400

    add base data cleaning code before refactoring

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,6 +1,100 @@
+# glob list files given a certain extension in a directory
+from glob import glob
 import pandas as pd
-import matplotlib as plt
 
-df = pd.Dataframe({"A": [1, 2, 3], "B": [1, 2, 3]})
+# --------------------------------------------------------------
+# Read single CSV file
+# --------------------------------------------------------------
 
-df2 = pd.DataFrame({"C": [1, 2, 3], "D": [1, 2, 3]})
+single_file_acc = pd.read_csv(
+    "../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv"
+)
+
+single_file_gyr = pd.read_csv(
+    "../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Gyroscope_25.000Hz_1.4.4.csv"
+)
+
+# --------------------------------------------------------------
+# List all data in data/raw/MetaMotion
+# --------------------------------------------------------------
+files = glob("../../data/raw/MetaMotion/*.csv")
+len(files)
+
+# --------------------------------------------------------------
+# Extract features from filename
+# --------------------------------------------------------------
+data_path = "../../data/raw/MetaMotion/"
+f = files[1]
+
+participant = f.split("-")[0].replace(data_path, "")
+label = f.split("-")[1]
+category = f.split("-")[2].rstrip("123").rstrip("_MetaWear_2019")
+
+df = pd.read_csv(f)
+
+df["participant"] = participant
+df["label"] = label
+df["category"] = category
+
+# --------------------------------------------------------------
+# Read all files
+# --------------------------------------------------------------
+
+acc_df = pd.DataFrame()
+gyr_df = pd.DataFrame()
+
+acc_set = 1
+gyr_set = 1
+
+for f in files:
+
+    participant = f.split("-")[0].replace(data_path, "")
+    label = f.split("-")[1]
+    category = f.split("-")[2].rstrip("123").rstrip("_MetaWear_2019")
+
+    df = pd.read_csv(f)
+
+    df["participant"] = participant
+    df["label"] = label
+    df["category"] = category
+    
+    if "Accelerometer" in f:
+        df["set"] = acc_set
+        acc_set += 1
+        acc_df = pd.concat([acc_df, df])
+        
+    if "Gyroscope" in f:
+        df["set"] = gyr_set
+        gyr_set += 1
+        gyr_df = pd.concat([gyr_df, df])
+
+# --------------------------------------------------------------
+# Working with datetimes
+# --------------------------------------------------------------
+acc_df.info()
+
+acc_df.index = pd.to_datetime(acc_df["epoch (ms)"], unit="ms")
+gyr_df.index = pd.to_datetime(gyr_df["epoch (ms)"], unit="ms")
+
+
+# --------------------------------------------------------------
+# Turn into function
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Merging datasets
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Resample data (frequency conversion)
+# --------------------------------------------------------------
+
+# Accelerometer:    12.500HZ
+# Gyroscope:        25.000Hz
+
+
+# --------------------------------------------------------------
+# Export dataset
+# --------------------------------------------------------------
